7e2c8421a1361da79826c4eafaa5fe60afe7248a
Marc <marc.dhaubenstock@gmail.com>
2017-04-23 18:20:51 +0200

    completed task 1

---
+++ stereo_vision/main.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <opencv2\opencv.hpp>
+
+
+int ex_1();
+int ex_2();
+
+void computeCostVolume(const cv::Mat &imgLeft, const cv::Mat &imgRight, std::vector<cv::Mat> &costVolumeLeft, std::vector<cv::Mat> &costVolumeRight,
+	int windowSize, int disp);
+
+void compute_cost(cv::Mat &target, const cv::Mat &imgLeft, const cv::Mat &imgRight, int r, int c, int max_rows, int max_cols, int windowSize, int disp);
